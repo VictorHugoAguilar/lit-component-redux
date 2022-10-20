3c8df25cd8fbaa121234ff0f0d0693c98fc92bc6
Victor Hugo Aguilar <victorhugoaguilar@aol.com>
2022-10-20 13:13:26 +0200

    Add files via upload

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,18 @@
+export const incrementarContador = () => {
+    return {
+        type: "INCREMENT",
+    };
+};
+
+export const decrementarContador = () => {
+    return {
+        type: "DECREMENT",
+    };
+};
+
+export const cambiarAppName = (name) => {
+    return {
+        type: "CHANGE_APP_NAME",
+        name,
+    };
+};
